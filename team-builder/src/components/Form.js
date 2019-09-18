9fb1e19f237940bfc3000d4d9bd067cc8f263dba
Joshua Holtsclaw <Jildsmiles@yahoo.com>
2019-09-18 16:15:18 -0400

    Forms added. setting up onchange and onsubmit

--- team-builder/src/components/Form.js
+++ team-builder/src/components/Form.js
@@ -1,14 +1,29 @@
 import React, {useState} from 'react';
 
 const Form = props => {
+    const [newMem, setNewMem] = useState([{
+        name:"",
+        email:"",
+        role:""
+    }]);
+    const change = e =>{
+        setNewMem({...newMem, [event.target.name]: event.target.value});
+    }
+
+    const Submit = e =>{
+        e.preventDefault();
+        props.addNewMember(newMem);
+        setNewMem({name:"",email:"",role:""});
+    };
+
     return (
-        <form>
+        <form onSubmit = {Submit}>
             <label htmlFor="name">Name</label>
-            <input id="name" type="text" name="name"></input>
+            <input id="name" type="text" name="name" value={newMem.name} onChange={change}></input>
             <label htmlFor="email">Email</label>
-            <input id="email" type="text" name="name"></input>
+            <input id="email" type="text" name="name" value={newMem.email} onChange={change}></input>
             <label htmlFor="role">Role</label>
-            <input id="role" type="text" name="name"></input>
+            <input id="role" type="text" name="name" value={newMem.role} onChange={change}></input>
             <button type="submit">Add Member</button>
         </form>
     )
